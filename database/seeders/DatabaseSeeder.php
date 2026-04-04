80bfc49e5fb3fdd9eff29dfe5e222a09bac483a9
FernandoPerez25 <luiguifatuo98@gmail.com>
2026-04-03 19:56:16 -0600

    Fix: Autoload Seeders generado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,11 +4,12 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\RolesSeeder; 
+use Database\Seeders\AdminUserSeeder; 
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
-
     public function run(): void
     {
         $this->call([
